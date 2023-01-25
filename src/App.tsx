039a659fafde3b3a90ac0febab8d6abdf84da265
vanim <vanimouzad123@gmail.com>
2023-01-25 12:10:51 -0300

    :sparkles: feat: implements Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import rocketLogo from './assets/rocket.svg';
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <header className={styles.header}>
+      <img src={rocketLogo} alt="Logotipo de foguete" />
+      <p className={styles.textTo}>to</p>
+      <p className={styles.textDo}>do</p>
+    </header>
   )
 }
