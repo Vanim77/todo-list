ec83679a8e5e44090beba302fd26af4fe7070364
vanim <vanimouzad123@gmail.com>
2023-01-25 13:25:33 -0300

    :sparkles: feat: implements new task bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import rocketLogo from './assets/rocket.svg';
+import plusIcon from './assets/plus-icon.svg';
 import styles from './App.module.css';
 import './global.css';
 
 export function App() {
 
   return (
-    <header className={styles.header}>
-      <img src={rocketLogo} alt="Logotipo de foguete" />
-      <p className={styles.textTo}>to</p>
-      <p className={styles.textDo}>do</p>
-    </header>
+    <div>
+      <header className={styles.header}>
+        <img src={rocketLogo} alt="Logotipo de foguete" />
+        <p className={styles.textTo}>to</p>
+        <p className={styles.textDo}>do</p>
+      </header>
+      <div className={styles.newTaskBar}>
+        <input placeholder="Adicione uma nova tarefa" type="text" />
+        <div className={styles.newTaskButton}>
+          <button>Criar</button>
+          <img src={plusIcon} alt="símbolo de adição" />
+        </div>
+      </div>
+    </div>
   )
 }
