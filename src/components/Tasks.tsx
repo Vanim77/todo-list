a30583bd93206ebae39aba5af95cba594fcf3589
vanim <vanimouzad123@gmail.com>
2023-01-26 13:31:48 -0300

    :sparkles: feat:  New task

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,16 +1,57 @@
 import { PlusCircle, ClipboardText, Trash } from 'phosphor-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 
 import styles from './Tasks.module.css';
 
+interface ITask {
+  id: string;
+  description: string;
+  isChecked: boolean;
+}
+
 export function Tasks() {
+  const [newTaskText, setNewTaskText] = useState('');
+  const [tasks, setTasks] = useState([] as ITask[]);
+
+  const isTaskContentEmpty = newTaskText.length === 0;
+
+  function handleNewTaskContent(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+  }
+
+  function handleNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    setTasks([...tasks,
+      {
+        id: uuidv4(),
+        description: newTaskText,
+        isChecked: false
+      }
+    ]);
+
+    setNewTaskText('');
+  }
+
   return (
     <>
-      <form className={styles.form}>
-        <input placeholder="Adicione uma nova tarefa" type="text" />
-        <div className={styles.newTaskButton}>
-          <button type="submit">Criar</button>
+      <form onSubmit={handleNewTask} className={styles.form}>
+        <input
+          onChange={handleNewTaskContent}
+          placeholder="Adicione uma nova tarefa"
+          type="text"
+          value={newTaskText}
+        />
+        <label className={styles.newTaskButton}>
+          <button
+            disabled={isTaskContentEmpty}
+            type="submit"
+          >
+            Criar
+          </button>
           <PlusCircle size={20} />
-        </div>
+        </label>
       </form>
       <main>
         <div className={styles.tasksContainer}>
@@ -34,18 +75,26 @@ export function Tasks() {
         </div>
 
         <section>
-          {/* <div className={styles.emptyTasks}>
-            <ClipboardText size={56} />
-            <p>Você ainda não tem tarefas cadastradas</p>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-          </div> */}
-          <ul className={styles.taskList}>
-            <li className={styles.singleTask}>
-              <input type="checkbox" />
-              <p>Limpar a casa</p>
-              <Trash size={24} />
-            </li>
-          </ul>
+          {tasks.length === 0
+            ?
+              <div className={styles.emptyTasks}>
+                <ClipboardText size={56} />
+                <p>Você ainda não tem tarefas cadastradas</p>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </div>
+            :
+              <ul className={styles.taskList}>
+                {tasks.map(task => {
+                  return (
+                    <li key={task.id} className={styles.singleTask}>
+                      <input type="checkbox" />
+                      <p>{task.description}</p>
+                      <Trash size={24} />
+                    </li>
+                  );
+                })}
+              </ul>
+          }
         </section>
       </main>
     </>
